bb865d26834ef4f27934c39b7fc4f1c3cb56fe93
Samwel Opiyo <samonyango20@gmail.com>
2017-05-15 22:14:11 +0300

    Static

--- Greenshoe/urls.py
+++ Greenshoe/urls.py
@@ -27,3 +27,11 @@ urlpatterns = [
     url(r'^signup/$', RedirectView.as_view(url='/accounts/signup/')),
     url(r'^help/$', TemplateView.as_view(template_name="help.html"), name='help'),
 ]
+
+from django.conf import settings
+from django.views import static
+
+if settings.DEBUG :
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', static.serve, {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    ]
